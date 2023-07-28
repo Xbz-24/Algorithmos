674a517c427f2d72064da3cebb6a8603b62e3174
renato <renatochavez7@gmail.com>
2023-07-29 01:47:51 +0200

    changes to treaptree

--- src/datastructures/balancedtree/TreapTree.hs
+++ src/datastructures/balancedtree/TreapTree.hs
@@ -1,4 +1,12 @@
 module Treap (Treap, empty, insert, delete, member, size) where
 
 import System.Random (StdGen, randomR, mkStdGen)
-import
+import Data.List (fold')
+
+data Treap a = Empty | Node a Int (Treap a) (Treap a) deriving (Show, Eq)
+
+empty :: Treap a
+empty = Empty
+
+size :: Treap a -> Int
+size 
