a4f3a4e03b6d7798aef7ae258d0fc4a982a638b6
renato <renatochavez7@gmail.com>
2023-07-29 01:55:34 +0200

    updated TreapTree.hs

--- src/datastructures/balancedtree/TreapTree.hs
+++ src/datastructures/balancedtree/TreapTree.hs
@@ -9,4 +9,18 @@ empty :: Treap a
 empty = Empty
 
 size :: Treap a -> Int
-size 
+size Empty
+size (Node _ _ l r) = 1 + size 1 + size r
+
+member :: (Ord a) => a -> Treap a -> Bool
+member _ Empty = False
+member x (Node a _ left right)
+    | x < a      = member x left
+    | x > a      = member x right
+    | otherwise  = True
+
+rotateLeft :: Treap a -> Treap a
+rotateLeft (Node a p (Node b q t1 t2) t3) = Node b q t1 (Node a p t2 t3)
+
+rotateRight :: Treap a -> Treap a
+rotateRight
